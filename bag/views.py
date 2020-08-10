01558928a4d1af620bc829b57dbc4fc205736460
tjwjobs <tjwjobs@yahoo.com>
2020-08-10 15:34:15 +0000

    add to bag

--- bag/views.py
+++ bag/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render, redirect
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
-# Create your views here.
+def bag_contents(request):
 
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
-
-def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    bag_items = []
+    total = 0
+    product_count = 0
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

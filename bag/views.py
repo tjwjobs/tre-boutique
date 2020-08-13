644a0accfdfce583a3026185d72a6e69e8583dbd
tjwjobs <tjwjobs@yahoo.com>
2020-08-13 14:24:16 +0000

    finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -1,24 +1,35 @@
-from decimal import Decimal
-from django.conf import settings
-from django.shortcuts import get_object_or_404
-from products.models import Product
+from django.shortcuts import render, redirect
 
-def bag_contents(request):
+# Create your views here.
 
-    bag_items = []
-    total = 0
-    product_count = 0
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    request.session['bag'] = bag
+    return redirect(redirect_url)
